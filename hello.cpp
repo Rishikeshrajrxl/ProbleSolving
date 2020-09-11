6b9056d39083ab7e603a9dc2bc1f71c24260d431
Rishikeshrajrxl <rishikeshrajrxl@gmail.com>
2020-09-11 21:44:40 +0530

    hello_world

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+
+#include<bits/stdc++.h>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<"Hello World";
+    return 0;
+}
+// to compile the code : g++ file name with .cpp
+// to run the compiled file ./a.exe
